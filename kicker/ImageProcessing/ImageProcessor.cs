47da28ecf0dda5b82c420737f6e3e5be2f897a8d
Andreas Koenig <andreas.koenig@st.oth-regensburg.de>
2020-02-21 14:42:47 +0100

    Finish basic version of soccer court detection

--- kicker/ImageProcessing/ImageProcessor.cs
+++ kicker/ImageProcessing/ImageProcessor.cs
@@ -12,8 +12,17 @@ namespace ImageProcessing
     public class ImageProcessor : BaseVideoProcessor, IImageProcessor
     {
         private const string ChannelThreshold = "threshold";
-        private const string ChannelEdges = "edges";
         private const string ChannelBoxes = "boxes";
+        private const string ChannelDetections = "detections";
+        private const string ChannelField = "field";
+
+        private const int TouchlineMaxYDeviation = 20;
+        private const int TouchLineSegmentMinAspectRatio = 15;
+
+        private readonly Scalar _red = new Scalar(0, 0, 255);
+        private readonly Scalar _yellow = new Scalar(255, 255, 0);
+        private readonly Scalar _green = new Scalar(0, 255, 0);
+        private readonly Scalar _blue = new Scalar(255, 0, 0);
 
         private readonly IWritableOptions<ImageProcessorSettings> _options;
 
@@ -30,8 +39,9 @@ namespace ImageProcessing
             return new Channel[]
             {
                 new Channel(ChannelThreshold, "Threshold Image", "The binarized image"),
-                new Channel(ChannelEdges, "Edges", "The edge detection algorithm applied to the threshold image"),
                 new Channel(ChannelBoxes, "Bounding Boxes", "The edge image with bounding boxes displayed"),
+                new Channel(ChannelDetections, "Detections", "The detected touchline segements and corners"),
+                new Channel(ChannelField, "Playing Field", "The detected playing field"),
             };
         }
 
@@ -41,47 +51,57 @@ namespace ImageProcessing
 
             try
             {
-                var close_kernel = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(_options.Value.DilationIterations, _options.Value.DilationIterations));
-
                 // Mask white lines (includes bars)
                 var markings = _options.Value.FieldMarkings;
                 var lower = HsvToScalar(markings.Lower);
                 var upper = HsvToScalar(markings.Upper);
                 var threshImg = img.CvtColor(ColorConversionCodes.BGR2HSV)
-                    .InRange(lower, upper);/*
-                    .GaussianBlur(new Size(_options.Value.BlurSize, _options.Value.BlurSize), 0)
-                    .MorphologyEx(MorphTypes.Close, close_kernel);*/
+                    .InRange(lower, upper);
 
                 frame.Release();
 
-                // Find and draw contours
-                //var edgeImg = threshImg.Sobel(threshImg.Type(), 1, 0);
-                var edgeImg = threshImg.Canny(1, 3);
-
-                edgeImg.FindContours(out Point[][] contours, out HierarchyIndex[] hierarchy,
+                // Find contours
+                threshImg.FindContours(out Point[][] contours, out HierarchyIndex[] hierarchy,
                     RetrievalModes.External, ContourApproximationModes.ApproxTC89L1);
-                edgeImg = edgeImg.CvtColor(ColorConversionCodes.GRAY2BGR);
-                //edgeImg.DrawContours(contours, -1, new Scalar(0, 255, 255), 1);
 
-                var rects = GetBoundingRects(contours);
-                var avgArea = rects.Sum(rect => Area(rect)) / rects.Count();
-                var avgAspectRatio = rects.Sum(rect => AspectRatio(rect)) / rects.Count();
-                var longRects = rects.Where(rect => AspectRatio(rect) >= avgAspectRatio);
-                var bigRects = rects.Where(rect => Area(rect) >= avgArea);
+                // Get bboxes and filter out the small ones that are irrelevant
+                var boundingBoxes = GetBoundingRects(contours)
+                    .Where(rect => Area(rect) > _options.Value.MinimalBboxArea);
+
+                // Get soccer court elements
+                var corners = GetCorners(boundingBoxes);
+                var touchlines = GetTouchlines(boundingBoxes);
+                var goals = GetGoals(boundingBoxes);
+                var halfWayLines = GetHalfWayLine(threshImg.Size(), boundingBoxes);
+
+                threshImg = threshImg.CvtColor(ColorConversionCodes.GRAY2BGR);
 
                 switch (Channel?.Id)
                 {
                     case ChannelThreshold:
                         return new Frame(threshImg);
-                    case ChannelEdges:
-                        return new Frame(edgeImg);
-                    case ChannelBoxes:
-                        foreach (var rect in longRects)
-                        {
-                            edgeImg.Rectangle(rect, new Scalar(0, 0, 255), 2);
-                        }
 
-                        return new Frame(edgeImg);
+                    case ChannelBoxes:
+                        boundingBoxes.ToList().ForEach(rect => threshImg.Rectangle(rect, _red, 2));
+                        return new Frame(threshImg);
+
+                    case ChannelDetections:
+                        touchlines.Item1.ForEach(rect => threshImg.Rectangle(rect, _yellow, 2));
+                        touchlines.Item2.ForEach(rect => threshImg.Rectangle(rect, _yellow, 2));
+                        new List<Point> { corners.Item1, corners.Item2, corners.Item3, corners.Item4 }
+                            .ForEach(p => threshImg.Rectangle(new Rect(p.X - 3, p.Y - 3, 6, 6), _red, 2));
+                        new List<Rect> { goals.Item1, goals.Item2 }
+                            .ForEach(rect => threshImg.Rectangle(rect, _green, 2));
+                        new List<Rect> { halfWayLines.Item1, halfWayLines.Item2 }
+                            .ForEach(rect => threshImg.Rectangle(rect, _blue, 2));
+
+                        return new Frame(threshImg);
+
+                    case ChannelField:
+                        var transform = GetPerspectiveTransform(halfWayLines, corners);
+                        Cv2.WarpPerspective(threshImg, threshImg, transform, threshImg.Size());
+                        return new Frame(threshImg);
+
                     default:
                         return new Frame(threshImg);
                 }
@@ -114,6 +134,83 @@ namespace ImageProcessing
             };
         }
 
+        private int GetMiddle(Point p1, Point p2, Func<Point, int> f)
+        {
+            if (f(p1) > f(p2))
+            {
+                return f(p2) + (f(p1) - f(p2));
+            }
+
+            return f(p1) + (f(p2) - f(p1));
+        }
+
+        private (Point, Point, Point, Point) GetCorners(IEnumerable<Rect> boxes)
+        {
+            var leftRects = boxes
+                    .OrderBy(rect => rect.X)
+                    .Take(3)
+                    .OrderBy(rect => rect.Y);
+            var topLeft = leftRects.First().TopLeft;
+            var bottomLeft = new Point(leftRects.Last().X, leftRects.Last().Bottom);
+
+            var rightRects = boxes
+                .OrderBy(rect => rect.Right)
+                .TakeLast(3)
+                .OrderBy(rect => rect.Y);
+            var bottomRight = rightRects.Last().BottomRight;
+            var topRight = new Point(rightRects.First().Right, rightRects.First().Top);
+
+            return (topLeft, topRight, bottomRight, bottomLeft);
+        }
+
+        private (List<Rect>, List<Rect>) GetTouchlines(IEnumerable<Rect> boxes)
+        {
+            boxes = boxes
+                .OrderBy(rect => rect.Y)
+                .Where(rect => AspectRatio(rect) > TouchLineSegmentMinAspectRatio);
+
+            var upperTouchline = GetTouchline(boxes);
+            var lowerTouchline = GetTouchline(boxes.Reverse());
+
+            return (upperTouchline, lowerTouchline);
+        }
+
+        private List<Rect> GetTouchline(IEnumerable<Rect> boxes)
+        {
+            int oldY = boxes.First().Y;
+            return boxes
+                .TakeWhile(rect =>
+                {
+                    bool valid = Math.Abs(rect.Y - oldY) < TouchlineMaxYDeviation;
+                    oldY = rect.Y;
+                    return valid;
+                })
+                .OrderBy(rect => rect.X)
+                .ToList();
+        }
+
+        private (Rect, Rect) GetGoals(IEnumerable<Rect> boxes)
+        {
+            boxes = boxes.OrderBy(rect => rect.X);
+
+            return (boxes.First(), boxes.Last());
+        }
+
+        private (Rect, Rect) GetHalfWayLine(Size imgSize, IEnumerable<Rect> boxes)
+        {
+            var xCenter = imgSize.Width / 2;
+            var yCenter = imgSize.Height / 2;
+
+            boxes = boxes
+                .OrderBy(rect => Math.Abs(xCenter - (rect.Right - rect.Left)))
+                .Take(3)
+                .OrderBy(rect => Math.Abs(yCenter - (rect.Top - rect.Bottom)))
+                .TakeLast(2)
+                .OrderBy(rect => rect.Y);
+
+            return (boxes.ElementAt(0), boxes.ElementAt(1));
+        }
+
         private int Area(Rect rect)
         {
             return rect.Width * rect.Height;
@@ -122,6 +219,26 @@ namespace ImageProcessing
         private float AspectRatio(Rect rect)
         {
             return ((float)rect.Width) / rect.Height;
+        }
+
+        private Mat GetPerspectiveTransform((Rect, Rect) halfWayLines, (Point, Point, Point, Point) corners)
+        {
+            (var topHalfWay, var bottomHalfWay) = halfWayLines;
+            (var topLeft, var topRight, var bottomRight, var bottomLeft) = corners;
+
+            int yTop = topHalfWay.Top;
+            int yBottom = bottomHalfWay.Bottom;
+
+            var origPoints = new Point2f[] { topLeft, topRight, bottomRight, bottomLeft };
+            var newPoints = new Point2f[]
+            {
+                new Point(topLeft.X, yTop),
+                new Point(topRight.X, yTop),
+                new Point(bottomRight.X, yBottom),
+                new Point(bottomLeft.X, yBottom),
+            };
+
+            return Cv2.GetPerspectiveTransform(origPoints, newPoints);
         }
     }
 }
--- kicker/ImageProcessing/ImageProcessorSettings.cs
+++ kicker/ImageProcessing/ImageProcessorSettings.cs
@@ -12,15 +12,7 @@ namespace ImageProcessing
             0, 0, 220, 255, 120, 255)]
         public ColorRange FieldMarkings { get; set; } = new ColorRange(0, 0, 0, 0, 0, 0);
 
-        [NumberParameter("Blur Size", "The size of the WxH kernel for the gaussian blur.", 3, 0, 9, 2)]
-        public double BlurSize { get; set; } = 3;
-
-        [NumberParameter("Dilation Iterations", "The amount of iterations that the dilation operation is executed.",
-            1, 0, 10, 1)]
-        public double DilationIterations { get; set; } = 1;
-
-        [NumberParameter("ContourHierarchy", "The contours of the image are organized in a hierarchy. This is their level.",
-            10, 0, 20, 1)]
-        public double ContourHierarchy { get; set; } = 10;
+        [NumberParameter("Minimal BBox Area", "The minimal area a bounding box can have", 10, 1, 1000, 1)]
+        public double MinimalBboxArea { get; set; } = 10;
     }
 }
